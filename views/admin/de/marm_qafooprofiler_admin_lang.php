ad124ded71b07fde5b3c66ef1a66241cf44fc48f
Joscha Krug <krug@marmalade.de>
2014-07-23 11:35:24 +0200

    Added translations for the backend

---
+++ views/admin/de/marm_qafooprofiler_admin_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$sLangName  = "Deutsch";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array( 
+    'charset'                       => 'ISO-8859-15',
+    'SHOP_MODULE_sApiKey'           => '<a href="https://profiler.qafoolabs.com/" target=_blank">Ihr API Key des Qafoo Profilers. Diesen finden Sie in den Projekteinstellungen</a>. ',
+    'SHOP_MODULE_isDevMode'         => 'Entwicklermodus aktivieren. Zeichnet alle Requests auf und &uuml;bermittelt die Daten per cURL. Wird im Produktivmodus des Shops automatisch deaktivert.',
+);
---
+++ views/admin/en/marm_qafooprofiler_admin_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$sLangName  = "English";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array( 
+    'charset'                       => 'ISO-8859-15',
+    'SHOP_MODULE_sApiKey'           => '<a href="https://profiler.qafoolabs.com/" target=_blank">Your Qafoo Profiler API key. You will find it in the settings of the project.</a>. ',
+    'SHOP_MODULE_isDevMode'         => 'Enable Developermode. Will log all requests and transmit the data via cURL. Will be deactivated automaticly if the productive mode of the shop is activated.',
+);
