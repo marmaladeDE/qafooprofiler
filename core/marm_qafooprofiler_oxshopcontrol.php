dcd6de0aa0490062661da52e760a31ce7ab8455d
Joscha Krug <krug@marmalade.de>
2014-08-08 12:54:32 +0200

    Fixed namesapce

--- core/marm_qafooprofiler_oxshopcontrol.php
+++ core/marm_qafooprofiler_oxshopcontrol.php
@@ -14,7 +14,7 @@ if( false )
 {
     class marm_qafooprofiler_oxshopcontrol_parent extends oxShopControl{}
 }
-if(!class_exists('QafooLabs\Profiler\Backend'))
+if(!class_exists('\\QafooLabs\\Profiler\\Backend'))
 {
     include(oxRegistry::getConfig()->getModulesDir() . 'marm/qafooprofiler/libs/QafooProfiler.php');
 }
